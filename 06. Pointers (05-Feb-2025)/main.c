3b9af8fdee93e3a33118a07d85d24244031519bb
rubayatrifat <rubayatislam17@gmail.com>
2025-02-05 17:07:45 +0600

    Pointers

--- 06. Pointers (05-Feb-2025)/main.c
+++ 06. Pointers (05-Feb-2025)/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void calculateSq(int* num) {
+    *num = (*num) * (*num);
+    printf("square is : %d \n", *num);
+}
 
 int main() {
     int age = 45;
@@ -13,6 +17,19 @@ int main() {
     printf("%u \n", &pointer);
     printf("%d \n", *pointer);
     printf("%d \n", *(&age));
+
+    // Pointer to pointer
+    int **pttp = &pointer;
+    // Q:1 print value of i pointer to pointer
+    int i = 1;
+    int *p = &i;
+    int **ptp = &p;
+    printf("%d \n", **ptp);
+
+    // Call by reference function using pointer
+    int num = 5;
+    calculateSq(&num);
+    printf("%d", num);
 
     return 0;
 }
